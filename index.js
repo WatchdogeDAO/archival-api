0ae1a327ed53b48f25b9c5ba21c473bbf172bf5e
Mauro <mauro@osn.global>
2020-07-26 00:39:50 -0300

    Pin to pinata the messages

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const axios = require('axios');
 const dotenv = require('dotenv');
+var bodyParser = require('body-parser');
 var cors = require('cors');
 const { runBot } = require('./src/bot');
 const { getTweets } = require('./src/db');
@@ -10,10 +12,30 @@ const app = express();
 const port = 3001;
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/tweets', async (req, res) => {
   const tweets = getTweets();
   res.json(tweets);
+});
+
+app.post('/pin', async (req, res) => {
+  const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+  try {
+    const response = await axios({
+      method: 'post',
+      url: url,
+      data: req.body.data,
+      headers: {
+        pinata_api_key: process.env.PINATA_KEY,
+        pinata_secret_api_key: process.env.PINATA_SECRET_KEY,
+      },
+    });
+    console.log(response.data.ipfsHash);
+    res.json(response.data);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 runBot();
